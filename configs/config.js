97e7443f130a8ee6d3b41eb7ee4f314a348ab69b
ajay7yadav95 <ajay7yadav95@gmail.com>
2022-06-03 21:11:48 +0530

    project

---
+++ configs/config.js
@@ -0,0 +1,10 @@
+// configration to db
+module.exports = {
+
+    HOST     : "localhost",          //  where we host
+    DB       : "healthiswealth",     //  data-base name
+    USER     : "ajay7",              //  
+    PASSWORD : "mysql@A7",           //
+    dialect  : "mysql"               //  which data base 
+
+}
